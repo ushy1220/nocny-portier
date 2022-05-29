4a560c2dad379bc1bcbbe7142a53cb207090f930
Michał Bielecki <mb9246@gmail.com>
2022-05-29 21:23:11 +0200

    add top floor

---
+++ script.js
@@ -0,0 +1,6 @@
+var td = document.querySelectorAll("td"); // Możesz użyć swojego sposobu pobierania elementów
+for (let i=0; i<td.lenght; i++) {
+li[i].onclick = function () {
+// Kod zmiany koloru
+};
+}
