31d8e1f3b802c3ac74ad68cbdb3ed2971062c8da
Michał Bielecki <mb9246@gmail.com>
2022-06-01 11:41:47 +0200

    change hooker color

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function change_color() {
-	var color = document.getElementById("table7").style.color;
-	if (color == "red"){
-		document.getElementById("aaa").style.color="black";
-	  	document.getElementById("aaa").style.backgroundColor="transparent";
-	}            
-	else {
-                document.getElementById("aaa").style.color="red";
-                document.getElementById("aaa").style.backgroundColor="blue";
+'use strict'
 
-	}
-}
+var hooker = document.querySelector('.hooker');
+var table = document.getElementsByClassName('table');
+
+hooker.addEventListener("click", function(){
+    document.querySelector('.hooker').className = "red"
+})
